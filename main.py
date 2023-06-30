ce6d9d0f3f92fa0e537b749652e4140443def807
Rohan <rohanmacbook@vsce.com>
2023-07-01 00:48:46 +0400

    GUI generation

---
+++ main.py
@@ -0,0 +1,26 @@
+
+#Module Importation 
+from tkinter import  *
+from tkinter import ttk 
+
+
+#Code Starts!
+class student():
+    def __init__(self,root):
+        self.root = root 
+        self.root.title("Student Database Management System")
+        self.root.geometry("1370x700+0+0")
+        self.root.resizable(False,False)
+
+        title = Label(self.root,text="Student Management System",bd=9,relief =GROOVE,font=("times new roman",50,"bold"),bg="green",fg="yellow")
+        title.pack(side=TOP,fill=X)
+
+        #Variables
+
+
+class student():
+    pass
+    root = Tk()
+    obj= student(root)
+    root.mainloop()
+
