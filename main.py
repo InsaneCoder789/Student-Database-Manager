472ab816ae9acde90a965888df641875950314c7
Rohan <rohanmacbook@vsce.com>
2023-07-02 15:23:26 +0530

    Updated screen ratio to fit buttons

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ class StudentManagementSystem:
             self.connection = mysql.connector.connect(
                 host="localhost",
                 user="root",
-                password="Rahul@5111",
+                password="Your_Password",
                 database="studentms"
             )
             self.cursor = self.connection.cursor()
         except Error as e:
             print("Error occurred while connecting to database:", e)
             messagebox.showerror("Error", "Failed to connect to database")
+            root.destroy()
 
         # Student Management System UI
         self.Roll_No_var = tk.StringVar()
@@ -61,178 +62,163 @@ class StudentManagementSystem:
         gender_label = tk.Label(details_frame, text="Gender", font=("Arial", 12), bg="light blue", fg="black")  # Change background and foreground color here
         gender_label.grid(row=4, column=0, padx=10, pady=5, sticky="w")
 
-        gender_combo = tk.Entry(details_frame, textvariable=self.gender_var, font=("Arial", 12), bd=2, relief=tk.GROOVE)
+        gender_combo = ttk.Combobox(details_frame, textvariable=self.gender_var, font=("Arial", 12), state="readonly")
+        gender_combo["values"] = ("Male", "Female", "Other")
         gender_combo.grid(row=4, column=1, padx=10, pady=5)
 
+        contact_label = tk.Label(details_frame, text="Contact", font=("Arial", 12), bg="light blue", fg="black")  # Change background and foreground color here
+        contact_label.grid(row=1, column=2, padx=10, pady=5, sticky="w")
+
+        contact_entry = tk.Entry(details_frame, textvariable=self.contact_var, font=("Arial", 12), bd=2, relief=tk.GROOVE)
+        contact_entry.grid(row=1, column=3, padx=10, pady=5)
+
+        dob_label = tk.Label(details_frame, text="Date of Birth", font=("Arial", 12), bg="light blue", fg="black")  # Change background and foreground color here
+        dob_label.grid(row=2, column=2, padx=10, pady=5, sticky="w")
+
+        dob_entry = tk.Entry(details_frame, textvariable=self.dob_var, font=("Arial", 12), bd=2, relief=tk.GROOVE)
+        dob_entry.grid(row=2, column=3, padx=10, pady=5)
+
+        add_btn = tk.Button(details_frame, text="Add", font=("Arial", 12, "bold"), bg="green", fg="white", command=self.add_students)  # Change background and foreground color here
+        add_btn.grid(row=5, column=0, padx=10, pady=5)
+
+        update_btn = tk.Button(details_frame, text="Update", font=("Arial", 12, "bold"), bg="blue", fg="white", command=self.update_students)  # Change background and foreground color here
+        update_btn.grid(row=5, column=1, padx=10, pady=5)
+
+        delete_btn = tk.Button(details_frame, text="Delete", font=("Arial", 12, "bold"), bg="red", fg="white", command=self.delete_students)  # Change background and foreground color here
+        delete_btn.grid(row=5, column=2, padx=10, pady=5)
+
+        clear_btn = tk.Button(details_frame, text="Clear", font=("Arial", 12, "bold"), bg="orange", fg="black", command=self.clear_entries)  # Change background and foreground color here
+        clear_btn.grid(row=5, column=3, padx=10, pady=5)
+
         # Student Table Frame
-        table_frame = tk.Frame(self.root, bd=4, relief=tk.RIDGE, bg="black")  # Change background color here
+        table_frame = tk.Frame(self.root, bd=4, relief=tk.RIDGE)
         table_frame.place(x=20, y=370, width=750, height=200)
 
         scroll_x = tk.Scrollbar(table_frame, orient=tk.HORIZONTAL)
         scroll_y = tk.Scrollbar(table_frame, orient=tk.VERTICAL)
-        self.Student_table = ttk.Treeview(table_frame, columns=("roll", "name", "email", "gender", "contact", "dob", "address"),
+
+        self.student_table = ttk.Treeview(table_frame, columns=("Roll No.", "Name", "Email", "Gender", "Contact", "DOB"),
                                           xscrollcommand=scroll_x.set, yscrollcommand=scroll_y.set)
-        
         scroll_x.pack(side=tk.BOTTOM, fill=tk.X)
         scroll_y.pack(side=tk.RIGHT, fill=tk.Y)
-        scroll_x.config(command=self.Student_table.xview)
-        scroll_y.config(command=self.Student_table.yview)
+        scroll_x.config(command=self.student_table.xview)
+        scroll_y.config(command=self.student_table.yview)
 
-        self.Student_table.heading("roll", text="Roll No.")
-        self.Student_table.heading("name", text="Name")
-        self.Student_table.heading("email", text="Email")
-        self.Student_table.heading("gender", text="Gender")
-        self.Student_table.heading("contact", text="Contact")
-        self.Student_table.heading("dob", text="Date of Birth")
-        self.Student_table.heading("address", text="Address")
-        self.Student_table['show'] = 'headings'
-        self.Student_table.column("roll", width=50)
-        self.Student_table.column("name", width=150)
-        self.Student_table.column("email", width=150)
-        self.Student_table.column("gender", width=50)
-        self.Student_table.column("contact", width=100)
-        self.Student_table.column("dob", width=100)
-        self.Student_table.column("address", width=200)
-        self.Student_table.pack(fill=tk.BOTH, expand=1)
-        self.Student_table.bind("<ButtonRelease-1>", self.get_cursor)
+        self.student_table.heading("Roll No.", text="Roll No.")
+        self.student_table.heading("Name", text="Name")
+        self.student_table.heading("Email", text="Email")
+        self.student_table.heading("Gender", text="Gender")
+        self.student_table.heading("Contact", text="Contact")
+        self.student_table.heading("DOB", text="DOB")
+        self.student_table["show"] = "headings"
 
-        self.fetch_data()
+        self.student_table.column("Roll No.", width=100)
+        self.student_table.column("Name", width=150)
+        self.student_table.column("Email", width=200)
+        self.student_table.column("Gender", width=100)
+        self.student_table.column("Contact", width=100)
+        self.student_table.column("DOB", width=100)
 
-        # Buttons Frame
-        button_frame = tk.Frame(self.root, bd=4, relief=tk.RIDGE, bg="light blue")  # Change background color here
-        button_frame.place(x=20, y=580, width=750, height=70)
+        self.student_table.pack(fill=tk.BOTH, expand=1)
+        self.student_table.bind("<ButtonRelease-1>", self.get_selected_row)
 
-        add_button = tk.Button(button_frame, text="Add", command=self.add_students, font=("Arial", 12), bg="light green")  # Change background color here
-        add_button.grid(row=0, column=0, padx=10, pady=10)
-
-        update_button = tk.Button(button_frame, text="Update", command=self.update_data, font=("Arial", 12), bg="light blue")  # Change background color here
-        update_button.grid(row=0, column=1, padx=10, pady=10)
-
-        delete_button = tk.Button(button_frame, text="Delete", command=self.delete_data, font=("Arial", 12), bg="red")  # Change background color here
-        delete_button.grid(row=0, column=2, padx=10, pady=10)
-
-        clear_button = tk.Button(button_frame, text="Clear", command=self.clear, font=("Arial", 12), bg="orange")  # Change background color here
-        clear_button.grid(row=0, column=3, padx=10, pady=10)
-
-        # Search Frame
-        search_frame = tk.LabelFrame(self.root, bd=4, relief=tk.RIDGE, bg="light blue")  # Change background color here
-        search_frame.place(x=20, y=330, width=750, height=35)
-
-        search_label = tk.Label(search_frame, text="Search By", font=("Arial", 12), bg="light blue", fg="black")  # Change background and foreground color here
-        search_label.grid(row=0, column=0, padx=10, pady=5)
-
-        search_combo = ttk.Combobox(search_frame, font=("Arial", 12), state='readonly', width=10)
-        search_combo['values'] = ("roll", "name", "email", "gender", "contact", "dob", "address")
-        search_combo.grid(row=0, column=1, padx=10, pady=5)
-
-        search_entry = tk.Entry(search_frame, font=("Arial", 12), bd=2, relief=tk.GROOVE)
-        search_entry.grid(row=0, column=2, padx=10, pady=5)
-
-        search_button = tk.Button(search_frame, text="Search", command=self.search_data, font=("Arial", 12), bg="light green")  # Change background color here
-        search_button.grid(row=0, column=3, padx=10, pady=5)
-
-        showall_button = tk.Button(search_frame, text="Show All", command=self.fetch_data, font=("Arial", 12), bg="light blue")  # Change background color here
-        showall_button.grid(row=0, column=4, padx=10, pady=5)
+        self.display_students()
 
     def add_students(self):
-        if self.Roll_No_var.get() == "" or self.name_var.get() == "":
-            messagebox.showerror("Error", "All fields are required")
+        roll_no = self.Roll_No_var.get()
+        name = self.name_var.get()
+        email = self.email_var.get()
+        gender = self.gender_var.get()
+        contact = self.contact_var.get()
+        dob = self.dob_var.get()
+
+        if roll_no == "" or name == "":
+            messagebox.showerror("Error", "Roll No. and Name are required fields")
         else:
             try:
-                query = "INSERT INTO students (roll_no, name, email, gender, contact, dob, address) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                values = (self.Roll_No_var.get(), self.name_var.get(), self.email_var.get(), self.gender_var.get(), self.contact_var.get(), self.dob_var.get(), self.txt_Address.get('1.0', tk.END))
-                self.cursor.execute(query, values)
+                self.cursor.execute("INSERT INTO students (roll_no, name, email, gender, contact, dob) VALUES (%s, %s, %s, %s, %s, %s)",
+                                    (roll_no, name, email, gender, contact, dob))
                 self.connection.commit()
-                self.fetch_data()
-                self.clear()
-                messagebox.showinfo("Success", "Record has been inserted")
+                messagebox.showinfo("Success", "Record added successfully")
+                self.clear_entries()
+                self.display_students()
             except Error as e:
                 print("Error occurred while adding student:", e)
                 messagebox.showerror("Error", "Failed to add student")
 
-    def fetch_data(self):
-        try:
-            self.cursor.execute("SELECT * FROM students")
-            rows = self.cursor.fetchall()
-            if len(rows) != 0:
-                self.Student_table.delete(*self.Student_table.get_children())
-                for row in rows:
-                    self.Student_table.insert("", tk.END, values=row)
-        except Error as e:
-            print("Error occurred while fetching data:", e)
-            messagebox.showerror("Error", "Failed to fetch data")
+    def update_students(self):
+        roll_no = self.Roll_No_var.get()
+        name = self.name_var.get()
+        email = self.email_var.get()
+        gender = self.gender_var.get()
+        contact = self.contact_var.get()
+        dob = self.dob_var.get()
 
-    def clear(self):
+        if roll_no == "":
+            messagebox.showerror("Error", "Please select a student from the table to update")
+        else:
+            try:
+                self.cursor.execute("UPDATE students SET name=%s, email=%s, gender=%s, contact=%s, dob=%s WHERE roll_no=%s",
+                                    (name, email, gender, contact, dob, roll_no))
+                self.connection.commit()
+                messagebox.showinfo("Success", "Record updated successfully")
+                self.clear_entries()
+                self.display_students()
+            except Error as e:
+                print("Error occurred while updating student:", e)
+                messagebox.showerror("Error", "Failed to update student")
+
+    def delete_students(self):
+        roll_no = self.Roll_No_var.get()
+
+        if roll_no == "":
+            messagebox.showerror("Error", "Please select a student from the table to delete")
+        else:
+            confirmation = messagebox.askyesno("Confirmation", "Do you want to delete this student?")
+            if confirmation == 1:
+                try:
+                    self.cursor.execute("DELETE FROM students WHERE roll_no=%s", (roll_no,))
+                    self.connection.commit()
+                    messagebox.showinfo("Success", "Record deleted successfully")
+                    self.clear_entries()
+                    self.display_students()
+                except Error as e:
+                    print("Error occurred while deleting student:", e)
+                    messagebox.showerror("Error", "Failed to delete student")
+
+    def clear_entries(self):
         self.Roll_No_var.set("")
         self.name_var.set("")
         self.email_var.set("")
         self.gender_var.set("")
         self.contact_var.set("")
         self.dob_var.set("")
-        self.txt_Address.delete("1.0", tk.END)
 
-    def get_cursor(self, event):
-        cursor_row = self.Student_table.focus()
-        contents = self.Student_table.item(cursor_row)
-        row = contents['values']
-        self.Roll_No_var.set(row[0])
-        self.name_var.set(row[1])
-        self.email_var.set(row[2])
-        self.gender_var.set(row[3])
-        self.contact_var.set(row[4])
-        self.dob_var.set(row[5])
-        self.txt_Address.delete("1.0", tk.END)
-        self.txt_Address.insert(tk.END, row[6])
-
-    def update_data(self):
+    def display_students(self):
         try:
-            query = "UPDATE students SET name=%s, email=%s, gender=%s, contact=%s, dob=%s, address=%s WHERE roll_no=%s"
-            values = (self.name_var.get(), self.email_var.get(), self.gender_var.get(), self.contact_var.get(), self.dob_var.get(), self.txt_Address.get('1.0', tk.END), self.Roll_No_var.get())
-            self.cursor.execute(query, values)
-            self.connection.commit()
-            self.fetch_data()
-            self.clear()
-            messagebox.showinfo("Success", "Record has been updated")
+            self.cursor.execute("SELECT * FROM students")
+            rows = self.cursor.fetchall()
+            self.student_table.delete(*self.student_table.get_children())
+            for row in rows:
+                self.student_table.insert("", tk.END, values=row)
         except Error as e:
-            print("Error occurred while updating student:", e)
-            messagebox.showerror("Error", "Failed to update student")
+            print("Error occurred while fetching students:", e)
+            messagebox.showerror("Error", "Failed to fetch students")
 
-    def delete_data(self):
-        if self.Roll_No_var.get() == "":
-            messagebox.showerror("Error", "Please select a record to delete")
-        else:
-            confirm = messagebox.askyesno("Confirm Delete", "Are you sure you want to delete this record?")
-            if confirm:
-                try:
-                    query = "DELETE FROM students WHERE roll_no=%s"
-                    value = (self.Roll_No_var.get(),)
-                    self.cursor.execute(query, value)
-                    self.connection.commit()
-                    self.fetch_data()
-                    self.clear()
-                    messagebox.showinfo("Success", "Record has been deleted")
-                except Error as e:
-                    print("Error occurred while deleting student:", e)
-                    messagebox.showerror("Error", "Failed to delete student")
-
-    def search_data(self):
-        if self.Roll_No_var.get() == "" and self.name_var.get() == "" and self.email_var.get() == "" and self.gender_var.get() == "" and self.contact_var.get() == "" and self.dob_var.get() == "":
-            messagebox.showerror("Error", "Please enter a search criteria")
-        else:
-            try:
-                query = "SELECT * FROM students WHERE " + self.search_combo.get() + " LIKE '%" + self.search_entry.get() + "%'"
-                self.cursor.execute(query)
-                rows = self.cursor.fetchall()
-                if len(rows) != 0:
-                    self.Student_table.delete(*self.Student_table.get_children())
-                    for row in rows:
-                        self.Student_table.insert("", tk.END, values=row)
-                else:
-                    messagebox.showinfo("No Results", "No matching records found")
-            except Error as e:
-                print("Error occurred while searching data:", e)
-                messagebox.showerror("Error", "Failed to search data")
+    def get_selected_row(self, event):
+        try:
+            row = self.student_table.focus()
+            content = self.student_table.item(row)
+            row = content["values"]
+            self.Roll_No_var.set(row[0])
+            self.name_var.set(row[1])
+            self.email_var.set(row[2])
+            self.gender_var.set(row[3])
+            self.contact_var.set(row[4])
+            self.dob_var.set(row[5])
+        except IndexError:
+            pass
 
 root = tk.Tk()
-student_management_system = StudentManagementSystem(root)
+obj = StudentManagementSystem(root)
 root.mainloop()
